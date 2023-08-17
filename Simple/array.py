9090b01b35bf94b1fc87a64a2b8635b3aa6819b9
artifactNU <simon.nilsson.oberg@gmail.com>
2023-08-17 06:55:02 -0700

    Update

--- Simple/array.py
+++ Simple/array.py
@@ -6,7 +6,7 @@ data = np.array([1, 2, 3, 4, 5])
 # Perform some basic operations
 mean = np.mean(data)
 total_sum = np.sum(data)
-square_root = np.sqrt(data)  # This will calculate the square root of each element
+square_root = np.sqrt(data)  # Calculate the square root of each element
 
 print("Original data:", data)
 print("Mean:", mean)
