126bf9bb980359ae3895d97f2024f385be091993
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-08-17 06:39:30 -0700

    Create

---
+++ Simple/array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create a simple array
+data = np.array([1, 2, 3, 4, 5])
+
+# Perform some basic operations
+mean = np.mean(data)
+total_sum = np.sum(data)
+square_root = np.sqrt(data)  # This will calculate the square root of each element
+
+print("Original data:", data)
+print("Mean:", mean)
+print("Sum:", total_sum)
+print("Square root:", square_root)
